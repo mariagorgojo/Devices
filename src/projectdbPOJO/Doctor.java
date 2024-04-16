10f214884cd2af2ea3d06c47a19f1b6ff2723f74
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-04-16 13:10:44 +0200

    changes

--- src/projectdbPOJO/Doctor.java
+++ src/projectdbPOJO/Doctor.java
@@ -3,4 +3,16 @@ package projectdbPOJO;
 public class Doctor {
 
 	//prueba 1
+	
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
