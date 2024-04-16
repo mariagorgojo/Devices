5c12cf26c1222aee885caaaa76ad0e5ab91750b1
anaisabel112 <anaisabel.deandresortega@usp.ceu.es>
2024-04-16 13:25:39 +0200

    name

--- src/projectdbPOJO/Doctor.java
+++ src/projectdbPOJO/Doctor.java
@@ -4,6 +4,16 @@ public class Doctor {
 
 	//prueba 1
 	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	private int id;
 
 	public int getId() {
