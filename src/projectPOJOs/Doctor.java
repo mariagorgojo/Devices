132274e34f1b7228c4294326461a72e462e5189b
sandraacabrera <sandra.cabreracamacho@usp.ceu.es>
2024-04-18 19:07:45 +0200

    pojo doctor

--- src/projectPOJOs/Doctor.java
+++ src/projectPOJOs/Doctor.java
@@ -1,8 +1,12 @@
 package projectPOJOs;
 //Hay que utilizar la clase Date. Importar librerías: seleccionar import java.sql.Date
 //añadir constructor, hashcode y equals
-public class Doctor {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
 
+public class Doctor implements Serializable{
+	private static final long serialVersionUID = -4423025978446522586L;
 	private int id;
 	private String name;
 	private String surname;
@@ -34,6 +38,29 @@ public class Doctor {
 
 	public String getSpeciality() {
 		return speciality;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, speciality, surname);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(speciality, other.speciality)
+				&& Objects.equals(surname, other.surname);
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [id=" + id + ", name=" + name + ", surname=" + surname + ", speciality=" + speciality + "]";
 	}
 
 	
