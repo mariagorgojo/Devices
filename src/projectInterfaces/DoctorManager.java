f7ebffffe9df00d2fc3567e7c977a4ea6894cf96
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-04-25 13:35:12 +0200

    jdbc manager

--- src/projectInterfaces/DoctorManager.java
+++ src/projectInterfaces/DoctorManager.java
@@ -1,8 +1,12 @@
 package projectInterfaces;
+
 import java.util.List;
 
+import projectPOJOs.Doctor;
+
 public interface DoctorManager {
-	public void createDoctor( Doctor d);
+	
+	public void createDoctor(Doctor d);
 	public List<Doctor> getListOfDoctors();
 	public Doctor searchDoctorById(Integer id);
 
--- src/projectJDBC/JDBCManager.java
+++ src/projectJDBC/JDBCManager.java
@@ -1,6 +1,122 @@
 package projectJDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCManager {
+	
+	private Connection c = null;
+	
+	public JDBCManager() {
+		try {
+			//corregir los paths
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/vetclinic.db");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			
+			System.out.print("Database Connection opened.");
+			this.createTables();
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e) {
+			System.out.print("Libraries not loaded");
+		}
+	}
+	
+	private void createTables() {
+		try {
+			
+			Statement stmt = c.createStatement();
+			
+			String sql = "CREATE TABLE doctors ("
+					+ "doctor_id INTEGER PRIMARY KEY,"
+					+ "name TEXT,"
+					+ "surname TEXT,"
+					+ "specialty TEXT "
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE patients ("
+					+ "patient_id INTEGER PRIMARY KEY,"
+					+ "name TEXT,"
+					+ "surname TEXT,"
+					+ "device TEXT"
+					+ "birthdate TEXT,"
+					+ "diagnosis TEXT,"
+					+ "doctor_id INTEGER,"
+					+ "FOREIGN KEY(doctor_id) REFERENCES doctors (doctor_id) "
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE devices ("
+					+ "device_id INTEGER PRIMARY KEY,"
+					+ "type TEXT,"
+					+ "implantation_date TEXT,"
+					+ "expiration_date TEXT,"
+					+ "manufacturer_id	INTEGER,"
+					+ "patient_id	INTEGER,"
+					+ "FOREIGN KEY(manufacturer_id) REFERENCES manufacturers(manufacturer_id),"
+					+ "FOREIGN KEY(patient_id) REFERENCES patients(patient_id) "
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE appointments ("
+					+ "appointment_id INTEGER PRIMARY KEY,"
+					+ "date TEXT,"
+					+ "description TEXT,"
+					+ "manufacturer_id	INTEGER,"
+					+ "patient_id	INTEGER,"
+					+ "FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id),"
+					+ "FOREIGN KEY(patient_id) REFERENCES patients(patient_id) "
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE manufacturers ("
+					+ "manufacturer_id	INTEGER PRIMARY KEY,"
+					+ "name	TEXT,"
+					+ "address	TEXT,"
+					+ "phonenumber	INTEGER"
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE device_doctor ("
+					+ "doctor_id	INTEGER,"
+					+ "device_id	INTEGER,"
+					+ "amount	INTEGER,"
+					+ "date	TEXT,"
+					+ "FOREIGN KEY(doctor_id) REFERENCES doctors,"
+					+ "FOREIGN KEY(device_id) REFERENCES device,"
+					+ "PRIMARY KEY(doctor_id,device_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			
+		}catch(SQLException e) {
+			if(!e.getMessage().contains("already exists")) 
+			{
+				e.printStackTrace();
+			}			
+		}
+		
+	}
+	
+	public Connection getConnection() {
+		return c;
+	}
+	
+	public void disconnect()
+	{
+		try {
+			c.close();
+		}
+		catch(SQLException e){ 
+			e.printStackTrace();
+		}		
+	}
 
 }
