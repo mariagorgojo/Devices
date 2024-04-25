1e300d7b2319835d0fcbed40567505692b5c8270
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-04-25 14:07:51 +0200

    jdbc doctor manager

--- src/projectJDBC/JDBCDoctorManager.java
+++ src/projectJDBC/JDBCDoctorManager.java
@@ -1,5 +1,106 @@
 package projectJDBC;
 
-public class JDBCDoctorManager {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import projectInterfaces.DoctorManager;
+import projectPOJOs.Doctor;
+
+// no sabemos que métodos son necesarios para cada entity :/
+
+public class JDBCDoctorManager implements DoctorManager {
+	
+	private JDBCManager manager;
+	
+	public JDBCDoctorManager (JDBCManager m) {
+		this.manager = m;
+	}
+
+	@Override
+	public void createDoctor(Doctor d) {
+		// TODO Auto-generated method stub
+		try {
+			String sql= "INSERT INTO doctors (name, surname, specialty)"
+						+ "VALUES (?,?,?)";
+			
+			// por qué no incluye el id??
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			prep.setString(1, d.getName());
+			prep.setString(2, d.getSurname());
+			prep.setString(3, d.getSpecialty());
+			
+			prep.executeUpdate();				
+					
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public List<Doctor> getListOfDoctors() {
+		// TODO Auto-generated method stub
+		List<Doctor> doctors= new ArrayList<Doctor>();
+		
+		try {
+			Statement stmt = manager.getConnection().createStatement();
+			String sql = "SELECT * FROM doctors";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next())
+			{
+				Integer id = rs.getInt("id");
+				String name = rs.getString("name");
+				String surname = rs.getString("surname");
+				String specialty = rs.getString("specialty");
+				
+				Doctor d = new Doctor (id, name, surname, specialty);
+				doctors.add(d);
+			}
+			
+			rs.close();
+			stmt.close();
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		
+		}
+		
+		
+		return doctors;
+	}
+
+	@Override
+	public Doctor searchDoctorById(Integer id) {
+		// TODO Auto-generated method stub
+		Doctor d = null;
+		
+		
+		try {
+			Statement stmt = manager.getConnection().createStatement();
+			String sql = "SELECT * FROM doctors WHERE id=" + id;
+		
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			Integer d_id = rs.getInt("id");
+			String name = rs.getString("name");
+			String surname = rs.getString("surname");
+			String specialty = rs.getString("specialty");
+			
+		    d = new Doctor (d_id, name, surname, specialty);
+		    
+		    rs.close();
+		    stmt.close();
+		    
+		}catch(Exception e) {e.printStackTrace();}
+		
+		
+		return d;
+	}
 }
--- src/projectPOJOs/Doctor.java
+++ src/projectPOJOs/Doctor.java
@@ -4,12 +4,29 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Doctor implements Serializable{
+	
 	private static final long serialVersionUID = -4423025978446522586L;
+	
 	private int id;
 	private String name;
 	private String surname;
-	private String speciality;
+	private String specialty;
 	
+	public Doctor(String name, String surname, String speciality) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.specialty = speciality;
+	}
+
+	public Doctor(int id, String name, String surname, String speciality) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.specialty = speciality;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -34,13 +51,13 @@ public class Doctor implements Serializable{
 		this.surname = surname;
 	}
 
-	public String getSpeciality() {
-		return speciality;
+	public String getSpecialty() {
+		return specialty;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name, speciality, surname);
+		return Objects.hash(id, name, specialty, surname);
 	}
 
 	@Override
@@ -52,13 +69,13 @@ public class Doctor implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Doctor other = (Doctor) obj;
-		return id == other.id && Objects.equals(name, other.name) && Objects.equals(speciality, other.speciality)
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(specialty, other.specialty)
 				&& Objects.equals(surname, other.surname);
 	}
 
 	@Override
 	public String toString() {
-		return "Doctor [id=" + id + ", name=" + name + ", surname=" + surname + ", speciality=" + speciality + "]";
+		return "Doctor [id=" + id + ", name=" + name + ", surname=" + surname + ", speciality=" + specialty + "]";
 	}
 
 	
