cffe92943f47e5c4ca3180094bd62d638cc21654
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-05-28 13:02:37 +0200

    xml

--- src/projectIfaces/XMLManager.java
+++ src/projectIfaces/XMLManager.java
@@ -10,3 +10,31 @@ public interface XMLManager {
 	public void manufacturer2xml(Integer id);
 	public Manufacturer xml2Manufacturer(File xml);
 }
+
+/* unmarshal
+ * // TODO Auto-generated method stub
+		Manufacturer m = null;
+		try {
+			JAXBContext jaxbC = JAXBContext.newInstance(Manufacturer.class);
+			Unmarshaller jaxbU = jaxbC.createUnmarshaller();
+			m = (Manufacturer) jaxbU.unmarshal(xml);			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+		return m;*/
+
+/*/
+ * marshal
+ * / TODO Auto-generated method stub
+		Patient p = null;
+		try {
+			JAXBContext jaxbC = JAXBContext.newInstance(Patient.class);
+			Unmarshaller jaxbU = jaxbC.createUnmarshaller();
+			p = (Patient) jaxbU.unmarshal(xml);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		*/
